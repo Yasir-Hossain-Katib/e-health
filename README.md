f84761ac9c0ca45f00e5806da1c62d093b8ce0f5
7777-Katib <85574228+7777-Katib@users.noreply.github.com>
2022-12-19 02:20:06 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# EHEALTH
+# e-health
---
+++ myapp/urls.py
@@ -0,0 +1,39 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.register,name="myapp"),
+
+    path('index/',views.index,name="index"),
+    
+    path('register/',views.register,name="register"),
+
+    path('login/',views.loginP,name="login"),
+
+    path('logout/',views.logoutU,name="logout"),
+
+    path('doctorSignUp/', views.doctorSignUp, name='doctorSignUp'),
+
+    
+
+    path('doctorLogin/', views.loginD, name='doctorLogin'),
+
+    path('doctorProfile/', views.doctorProfile, name='doctorProfile'),
+
+    path('bio/', views.bio, name='bio'),
+
+    path('payment/', views.payment, name='payment'),
+
+    path('success/', views.success, name='success'),
+
+    path('Pboard/', views.Pboard, name='Pboard'),
+
+    path('Dboard/', views.Dboard, name='Dboard'),
+
+    path('feedback/', views.feedback, name='feedback')
+
+    
+
+    
+] 
